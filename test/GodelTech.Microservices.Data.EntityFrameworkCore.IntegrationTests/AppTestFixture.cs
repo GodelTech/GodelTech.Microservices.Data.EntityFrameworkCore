f9c056c9dea1a2ec82f43dffe2898a9461b9aa0a
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-07-17 20:39:08 +0300

    improve tests

---
+++ test/GodelTech.Microservices.Data.EntityFrameworkCore.IntegrationTests/AppTestFixture.cs
@@ -0,0 +1,10 @@
+﻿using GodelTech.Microservices.Data.EntityFrameworkCore.Demo;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace GodelTech.Microservices.Data.EntityFrameworkCore.IntegrationTests
+{
+    public class AppTestFixture : WebApplicationFactory<Startup>
+    {
+
+    }
+}
---
+++ test/GodelTech.Microservices.Data.EntityFrameworkCore.Tests/DataInitializerOptionsTests.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+
+namespace GodelTech.Microservices.Data.EntityFrameworkCore.Tests
+{
+    public class DataInitializerOptionsTests
+    {
+        private readonly DataInitializerOptions _options;
+
+        public DataInitializerOptionsTests()
+        {
+            _options = new DataInitializerOptions();
+        }
+
+        [Fact]
+        public void ConnectionStringName_Get_Success()
+        {
+            // Arrange & Act & Assert
+            Assert.Equal("DefaultConnection", _options.ConnectionStringName);
+        }
+
+        [Fact]
+        public void ConnectionStringName_Set_Success()
+        {
+            // Arrange
+            var expectedResult = "Test ConnectionStringName";
+
+            // Act
+            _options.ConnectionStringName = expectedResult;
+
+            // Assert
+            Assert.Equal(expectedResult, _options.ConnectionStringName);
+        }
+
+        [Fact]
+        public void SqlServerHealthCheckName_Get_Success()
+        {
+            // Arrange & Act & Assert
+            Assert.Equal("sqlserver", _options.SqlServerHealthCheckName);
+        }
+
+        [Fact]
+        public void SqlServerHealthCheckName_Set_Success()
+        {
+            // Arrange
+            var expectedResult = "Test SqlServerHealthCheckName";
+
+            // Act
+            _options.SqlServerHealthCheckName = expectedResult;
+
+            // Assert
+            Assert.Equal(expectedResult, _options.SqlServerHealthCheckName);
+        }
+
+        [Fact]
+        public void EnableDatabaseMigration_Get_Success()
+        {
+            // Arrange & Act & Assert
+            Assert.False(_options.EnableDatabaseMigration);
+        }
+
+        [Fact]
+        public void EnableDatabaseMigration_Set_Success()
+        {
+            // Arrange && Act
+            _options.EnableDatabaseMigration = true;
+
+            // Assert
+            Assert.True(_options.EnableDatabaseMigration);
+        }
+    }
+}
